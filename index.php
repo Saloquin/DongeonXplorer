b44bf3f3ca37e4b315d429a51f1abe2db2965940
gille231 <22301465@etu.unicaen.fr>
2024-11-04 10:12:41 +0100

    init

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require 'autoload.php';
+
+class Router
+{
+    private $routes = [];
+    private $prefix;
+
+    public function __construct($prefix = '')
+    {
+        $this->prefix = trim($prefix, '/');
+    }
+
+    public function addRoute($uri, $controllerMethod)
+    {
+        $this->routes[trim($uri, '/')] = $controllerMethod;
+    }
+
+    public function route($url)
+    {
+        // Suppression du préfixe du début de l'URL
+        if ($this->prefix && strpos($url, $this->prefix) === 0) {
+            $url = substr($url, strlen($this->prefix) + 1);
+        }
+
+        // Suppression des barres obliques en trop
+        $url = trim($url, '/');
+
+
+        // Vérification de la correspondance de l'URL à une route définie
+        if (array_key_exists($url, $this->routes)) {
+            // Extraction du nom du contrôleur et de la méthode
+            list($controllerName, $methodName) = explode('@', $this->routes[$url]);
+
+            // Instanciation du contrôleur et appel de la méthode
+            $controller = new $controllerName();
+            $controller->$methodName();
+        } else {
+            // Gestion des erreurs (page 404, etc.)
+            require_once "views/404.php";
+        }
+    }
+}
+
+// Instanciation du routeur
+$router = new Router('DemoMVCAlpha');
+
+// Ajout des routes
+$router->addRoute('', 'HomeController@index'); // Pour la racine
+$router->addRoute('tasks', 'TaskController@index'); // Pour la racine
+$router->addRoute('404', '404Controller@index');
+$router->addRoute('caca', 'cacaController@index');
+// Appel de la méthode route
+$router->route(trim($_SERVER['REQUEST_URI'], '/'));
