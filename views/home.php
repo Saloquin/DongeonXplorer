f0463b1af8f2f9b1cad3380ff4f318afa4fb603f
clscf <clementsurcouf14@gmail.com>
2024-11-28 16:12:44 +0100

    ajout d'un fond d'ecran

--- views/home.php
+++ views/home.php
@@ -24,6 +24,12 @@
                         title: ['"Pirata One"', 'cursive'],
                         body: ['Roboto', 'sans-serif'],
                     },
+                    backgroundImage: {
+                        'background-home': "url('/DongeonXplorer/Images/icon/fond.jpg')",
+                    },
+                    backgroundSize: {
+                        'larger': '100%',  // Ajouter une taille plus grande
+                    },
                 },
             },
         };
@@ -31,33 +37,39 @@
     <title>Dungeon Xplorer</title>
 </head>
 
-<body class="bg-primary text-textMain font-body min-h-screen">
+<body class="bg-background-home bg-larger text-textMain font-body min-h-screen background-home bg-fixed">
 
     <!-- Navbar -->
-    <nav class="bg-navbarDark border-gray-200">
+    <nav class="bg-transparent relative">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="home" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="/DongeonXplorer/Images/icon/Logo.png" class="h-8" alt="Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">DungeonXplorer</span>
-            </a>
-            <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+            <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-300 rounded-lg md:hidden hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
                 </svg>
             </button>
-            <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-navbarDark">
+            <!-- Added ml-auto here to push the links to the right -->
+            <div class="hidden w-full md:block md:w-auto ml-auto" id="navbar-default">
+                <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 rounded-lg bg-opacity-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:bg-transparent">
                     <li>
-                        <a href="register" class="block py-2 px-3 text-gray-300 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">S'inscrire</a>
+                        <a href="register" class="block py-2 px-3 text-gray-300 rounded transition duration-300 ease-in-out hover:bg-gray-800 hover:bg-opacity-50 md:hover:bg-transparent md:hover:text-accentGold md:p-0">
+                            S'inscrire
+                        </a>
                     </li>
                     <li>
-                        <a href="login" class="block py-2 px-3 text-gray-300 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Se connecter</a>
+                        <a href="login" class="block py-2 px-3 text-gray-300 rounded transition duration-300 ease-in-out hover:bg-gray-800 hover:bg-opacity-50 md:hover:bg-transparent md:hover:text-accentGold md:p-0">
+                            Se connecter
+                        </a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
+
+    <!-- Logo Under Navbar -->
+    <div class="flex justify-center mt-8">
+        <img src="/DongeonXplorer/Images/icon/Logo.ico" alt="DungeonXplorer Logo" class="h-64" />
+    </div>
 
     <!-- Main Content -->
     <main class="bg-secondary p-8 rounded-lg shadow-lg w-full max-w-4xl mx-auto mt-8">
